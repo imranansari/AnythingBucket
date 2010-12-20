3563a04ae72d3f74ed651ef70450c7ce5e73ae85
Jonathan Wight <jwight@mac.com>
2010-12-19 18:12:38 -0800

    Added link to guide.

--- Server/push.sh
+++ Server/push.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# http://guide.couchdb.org/draft/managing.html
+
 USER=admin
 read -p "Password for ${USER}? " -s PASSWORD
 couchapp push "http://${USER}:${PASSWORD}@touchcode.couchone.com/anything-db"
