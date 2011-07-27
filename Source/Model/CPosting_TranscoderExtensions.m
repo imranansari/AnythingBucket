3a417dbd9a09e2482d6855034cc99bb50d44b0fb
Jonathan Wight <jwight@mac.com>
2011-07-26 23:50:51 -0700

    Safety checkin (cont)

--- Source/Model/CPosting_TranscoderExtensions.m
+++ Source/Model/CPosting_TranscoderExtensions.m
@@ -13,45 +13,7 @@
 
 + (NSSet *)keysForEncodingAsType:(NSString *)inType
     {
-//    return([NSSet setWithObjects:@"title", NULL]);
     return([NSSet setWithObjects:@"title", @"body", @"location", @"tags", @"created", NULL]);
     }
-
-//+ (NSValueTransformer *)valueTransformerForTranscodingKey:(NSString *)inKey asType:(NSString *)inType
-//    {
-//    if ([inKey isEqualToString:@"coordinate"])
-//        {
-//        TransformerBlock theBlock = ^(id inValue) {
-//            CLLocationCoordinate2D theCoordinate;
-//            [inValue getValue:&theCoordinate];
-//            return([NSDictionary dictionaryWithObjectsAndKeys:
-//                [NSNumber numberWithDouble:theCoordinate.latitude], @"latitude",
-//                [NSNumber numberWithDouble:theCoordinate.longitude], @"longitude",
-//                NULL]);
-//            };
-//        NSValueTransformer *theValueTransformer = [[[CBlockValueTransformer alloc] initWithBlock:theBlock reverseBlock:NULL] autorelease];
-//        return(theValueTransformer);
-//        }
-//    else
-//        {
-//        return(NULL);
-//        }
-//    }
-//
-//+ (NSPredicate *)filterPredicateForTranscodingKey:(NSString *)inKey asType:(NSString *)inType
-//    {
-//    if ([inKey isEqualToString:@"course"])
-//        {
-//        id theBlock = ^(CLLocation *evaluatedObject, NSDictionary *bindings) { return(evaluatedObject.course >= 0.0); };
-//        
-//        NSPredicate *thePredicate = [NSPredicate predicateWithBlock:theBlock];
-//        return(thePredicate);
-//        }
-//    else
-//        {
-//        return(NULL);
-//        }
-//
-//    }
 
 @end
